71e7b7dff8950c5dab5ebab1a57ee593f6e52d09
Ville Kankaanpää <ville.kankaanpaa@gmail.com>
2017-11-25 22:13:40 +0200

    Twitch login and integration
    Add scene for Twitch login
    Add Twitch integration controller

---
+++ Assets/Scripts/TwitchChannelSetter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TwitchChannelSetter : MonoBehaviour
+{
+	public GameObject channel;
+	public GameObject oAuth;
+
+	InputField channelInputField;
+	InputField oAuthInputField;
+
+	void Start ()
+	{
+		channelInputField = channel.GetComponent<InputField>();
+		oAuthInputField = oAuth.GetComponent<InputField>();
+
+		channelInputField.text = PlayerPrefs.GetString("TWITCH_CHANNEL", "");
+		oAuthInputField.text = PlayerPrefs.GetString("TWITCH_OAUTH", "");
+	}
+	
+	void OnGUI ()
+	{
+		if (oAuthInputField.isFocused && channelInputField.text != "" && oAuthInputField.text != "" && Input.GetKeyDown(KeyCode.Return))
+		{
+			PlayerPrefs.SetString("TWITCH_CHANNEL", channelInputField.text);
+			PlayerPrefs.SetString("TWITCH_OAUTH", oAuthInputField.text);
+			SceneManager.LoadScene(1);
+		}
+	}
+}
---
+++ Assets/Scripts/TwitchIntegrationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using NetIrc2;
+using NetIrc2.Events;
+
+public class TwitchIntegrationController : MonoBehaviour
+{
+	IrcClient ircClient;
+	string channel;
+
+	void Start ()
+	{
+		ircClient = new IrcClient();
+
+		ircClient.GotIrcError += new EventHandler<IrcErrorEventArgs>(PrintMessage);
+		ircClient.GotMessage += new EventHandler<ChatMessageEventArgs>(PrintMessage);
+		ircClient.GotWelcomeMessage += new EventHandler<SimpleMessageEventArgs>(PrintMessage);
+
+		channel = PlayerPrefs.GetString("TWITCH_CHANNEL", "sirmorland").ToLower().Trim();
+		string oAuth = PlayerPrefs.GetString("TWITCH_OAUTH", "z1h9xf43hxh10i566b5d82qvo2fwbv").Trim();
+
+		if(oAuth.IndexOf("oauth:") != 0)
+		{
+			oAuth = "oauth:" + oAuth;
+		}
+		
+		ircClient.Connect("irc.chat.twitch.tv", 6667);
+		ircClient.LogIn(channel, channel, channel, null, null, oAuth);
+		ircClient.Join("#" + channel);
+	}
+
+	void PrintMessage(object o, SimpleMessageEventArgs simpleMessageEventArgs)
+	{
+		Debug.Log("Welcome: " + simpleMessageEventArgs.Message.ToString());
+	}
+	void PrintMessage(object o, ChatMessageEventArgs chatMessageEventArgs)
+	{
+		Debug.Log("Message: " + chatMessageEventArgs.Sender + " - " + chatMessageEventArgs.Message);
+	}
+	void PrintMessage(object o, IrcErrorEventArgs ircErrorEventArgs)
+	{
+		Debug.Log("Error: " + ircErrorEventArgs.Data.ToString());
+	}
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			ircClient.Message("#" + channel, "Hello!");
+		}
+	}
+}
